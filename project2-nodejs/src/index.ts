64c247d82583db52439bb49b949360acbc7b7e12
Fernando Barbosa <fernandobarbosa1697@gmail.com>
2022-06-03 22:49:49 -0300

    feat: 🎸 add new routes to index

--- project2-nodejs/src/index.ts
+++ project2-nodejs/src/index.ts
@@ -1,11 +1,24 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from "cors";
+
 import fileRoute from './infra/routes/fileRoute';
+import folderRoute from './infra/routes/folderRoute';
 
 dotenv.config();
+
 const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Working!');
+})
 
 fileRoute(app);
+folderRoute(app);
+
+
 
 const port = process.env.PORT || 3000;
 
