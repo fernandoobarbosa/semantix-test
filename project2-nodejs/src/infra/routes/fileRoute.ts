af68a5c311b533855ff01985d048eae705bad0ef
Fernando Barbosa <fernandobarbosa1697@gmail.com>
2022-06-03 22:49:05 -0300

    feat: 🎸 file route

--- project2-nodejs/src/infra/routes/fileRoute.ts
+++ project2-nodejs/src/infra/routes/fileRoute.ts
@@ -1,8 +1,8 @@
 import express, { Request, Response } from 'express';
 
 const fileRoute = (app: express.Application) => {
-  app.get('/file', (req:Request, res:Response) => {
-    res.send('Hello world!');
+  app.post('/files', (req:Request, res:Response) => {
+    res.send(req.body);
   });
 };
 
