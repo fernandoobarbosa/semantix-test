d94970d84af3720548d42c2b2ed566fd680ccabc
Fernando Barbosa <fernandobarbosa1697@gmail.com>
2022-06-05 16:17:16 -0300

    feat: 🎸 go file api calls

--- project2-nodejs/src/utils/goFileApi.ts
+++ project2-nodejs/src/utils/goFileApi.ts
@@ -1,11 +1,35 @@
 import axios from "axios";
 
-export const folderCreation = async (name: string) => {
-  
-    const data = {
-      parentFolderId: process.env.PARENT_FOLDER_ID,
-      token: process.env.GO_FILE_TOKEN ,
-      name: name,
+export const createFolder = async (name: string) => {
+  const data = {
+    parentFolderId: process.env.PARENT_FOLDER_ID,
+    token: process.env.GO_FILE_TOKEN,
+    folderName: name,
+  };
+
+  const response = await axios.put("https://api.gofile.io/createFolder", data);
+  return response.data;
+};
+
+export const uploadFile = async (file: any, folderId: string) => {
+    const formdata = new FormData();
+    const blob = new Blob([file.file.data], { type: "text/xml" });
+
+    formdata.append("file", blob, file.file.name);
+    formdata.append("token", process.env.GO_FILE_TOKEN ?? "");
+    formdata.append("folderId", folderId);
+
+    const requestOptions: object = {
+      method: "POST",
+      body: formdata,
+      redirect: "follow",
     };
-    await axios.put("https://api.gofile.io/createFolder", data);
+
+    const uploadResponse = await fetch(
+      "https://store1.gofile.io/uploadFile",
+      requestOptions
+    );
+
+      const createdFile = await uploadResponse.json();
+      return createdFile.data;
 };
